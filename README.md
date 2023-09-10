5b9cce8a1ff777d88ae798ad95c0f243568c4a6f
moyotsukai <gyoshu.bowie@gmail.com>
2023-09-10 23:16:12 +0900

    add: README

---
+++ README.md
@@ -0,0 +1,25 @@
+## jsonファイルを結合し、CreateML用のデータ形式に直すフォーマッタ
+
+1. このリポジトリをクローン（ダウンロードする）  
+
+2. ファイル名を連番にする  
+- 名称変更したいjsonファイルを全選択→右クリック→「名称変更」  
+![](./document/doc1.png)  
+![](./document/doc2.png)  
+
+3. `index.ts`を開き、以下の項目を編集
+```ts
+//結合したいjsonファイルが入ったフォルダのパス
+const BASE_PATH = "/Users/owner/Downloads/KaraokeClassifierData/TrainingData/labeledImgData"
+//command + option + C でファイルのパスをコピーできるので便利
+
+//ファイルの数
+const NUM = 46
+```  
+
+4. ターミナルを開き、このプロジェクトのディレクトリに移動し、以下を実行  
+```
+npm run start
+```
+
+5. プロジェクトディレクトリ内に`output.json`というファイルが生成される。
--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import fs from "fs"
 
+const BASE_PATH = "/Users/owner/Downloads/KaraokeClassifierData/TrainingData/labeledImgData"
+const NUM = 46
+
 const format = async () => {
   //join
   let joinedData: Array<any> = []
-  for (let i = 0; i < 46; i++) {
-    const data: Array<any> = JSON.parse(await fs.readFileSync(`/Users/owner/Downloads/KaraokeClassifierData/TrainingData/labeledImgData/d${i + 1}.json`, 'utf8'))
+  for (let i = 0; i < NUM; i++) {
+    const data: Array<any> = JSON.parse(await fs.readFileSync(`${BASE_PATH}/d${i + 1}.json`, 'utf8'))
     joinedData = joinedData.concat(data)
   }
 
